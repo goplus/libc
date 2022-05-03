1813a7e4aa2fa076145cfe4eff3162601af32f78
xushiwei <x@goplus.org>
2022-05-03 08:12:02 +0800

    update c2go.cfg

---
+++ c2go/include/syscall_arch.h
@@ -0,0 +1,7 @@
+long __syscall0(long n);
+long __syscall1(long n, long a);
+long __syscall2(long n, long a, long b);
+long __syscall3(long n, long a, long b, long c);
+long __syscall4(long n, long a, long b, long c, long d);
+long __syscall5(long n, long a, long b, long c, long d, long e);
+long __syscall6(long n, long a, long b, long c, long d, long e, long f);
