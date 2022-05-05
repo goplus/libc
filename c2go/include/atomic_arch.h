b1a32ac657cedcdc749177226e147d16c6a95b1e
xushiwei <x@goplus.org>
2022-05-05 22:27:12 +0800

    atomic_arch.h, pthread_arch.h

---
+++ c2go/include/atomic_arch.h
@@ -0,0 +1,5 @@
+#define a_cas a_cas
+int a_cas(volatile int *p, int t, int s);
+
+#define a_cas_p a_cas_p
+void *a_cas_p(volatile void *p, void *t, void *s);
---
+++ c2go/include/pthread_arch.h
@@ -0,0 +1,6 @@
+uintptr_t __get_tp();
+
+#define TLS_ABOVE_TP
+#define GAP_ABOVE_TP 8
+
+#define MC_PC arm_pc
