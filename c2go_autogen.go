4b7afa7ab8e618af9836668d451667a44d31715b
xushiwei <x@goplus.org>
2022-05-06 23:16:42 +0800

    __syscall(1,4,6)

--- c2go_autogen.go
+++ c2go_autogen.go
@@ -18,15 +18,6 @@ func __ofl_lock() **struct__IO_FILE {
 func __stdio_seek(*struct__IO_FILE, int64, int32) int64 {
 	panic("notimpl")
 }
-func __syscall1(n int64, a int64) int64 {
-	panic("notimpl")
-}
-func __syscall4(n int64, a int64, b int64, c int64, d int64) int64 {
-	panic("notimpl")
-}
-func __syscall6(n int64, a int64, b int64, c int64, d int64, e int64, f int64) int64 {
-	panic("notimpl")
-}
 func __syscall_cp(int64, int64, int64, int64, int64, int64, int64) int64 {
 	panic("notimpl")
 }
--- syscall.go
+++ syscall.go
@@ -4,9 +4,27 @@ import "syscall"
 
 var g_r1 uintptr // TODO: goroutine safe
 
+func __syscall1(n int64, a int64) int64 {
+	var ret syscall.Errno
+	g_r1, _, ret = syscall.Syscall(uintptr(n), uintptr(a), 0, 0)
+	return int64(ret)
+}
+
 func __syscall3(n int64, a int64, b int64, c int64) int64 {
 	var ret syscall.Errno
 	g_r1, _, ret = syscall.Syscall(uintptr(n), uintptr(a), uintptr(b), uintptr(c))
+	return int64(ret)
+}
+
+func __syscall4(n int64, a int64, b int64, c int64, d int64) int64 {
+	var ret syscall.Errno
+	g_r1, _, ret = syscall.Syscall6(uintptr(n), uintptr(a), uintptr(b), uintptr(c), uintptr(d), 0, 0)
+	return int64(ret)
+}
+
+func __syscall6(n int64, a int64, b int64, c int64, d int64, e int64, f int64) int64 {
+	var ret syscall.Errno
+	g_r1, _, ret = syscall.Syscall6(uintptr(n), uintptr(a), uintptr(b), uintptr(c), uintptr(d), uintptr(e), uintptr(f))
 	return int64(ret)
 }
 
