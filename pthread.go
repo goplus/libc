c82129d5234eba5c6d1a9f3663e96c24fac8b3d1
xushiwei <x@goplus.org>
2022-05-08 16:10:16 +0800

    gtls => c2go_gtls

--- pthread.go
+++ pthread.go
@@ -7,16 +7,16 @@ import (
 )
 
 var (
-	gtls sync.Map
+	c2go_gtls sync.Map
 )
 
 func __pthread_self() *struct___pthread {
 	var self *struct___pthread
 	id := goid.Get()
-	ret, ok := gtls.Load(id)
+	ret, ok := c2go_gtls.Load(id)
 	if !ok {
 		self = &struct___pthread{tid: int32(id)}
-		gtls.Store(id, self)
+		c2go_gtls.Store(id, self)
 	} else {
 		self = ret.(*struct___pthread)
 	}
