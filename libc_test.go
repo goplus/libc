07a9b4dc1ba8f1e23f13f9c0994e8516dbd38d1d
xushiwei <x@goplus.org>
2022-05-14 19:48:20 +0800

    TestSnprintf

--- libc_test.go
+++ libc_test.go
@@ -27,6 +27,12 @@ func goSprintf(format *int8, args ...interface{}) string {
 	return gostring(&buf[0])
 }
 
+func goSnprintf(format *int8, args ...interface{}) (string, int32) {
+	var buf [1 << 12]int8
+	ret := Snprintf(&buf[0], 1<<12, format, args...)
+	return gostring(&buf[0]), ret
+}
+
 func TestPrintf(t *testing.T) {
 	n := Printf(C("Hello, world\n"))
 	if n != 13 {
@@ -37,6 +43,12 @@ func TestPrintf(t *testing.T) {
 func TestSprintf(t *testing.T) {
 	if s := goSprintf(C("Hi %7.1f\n"), 3.1415926); s != "Hi     3.1\n" {
 		t.Fatal("TestSprintf:", s)
+	}
+}
+
+func TestSnprintf(t *testing.T) {
+	if s, ret := goSnprintf(C("%f"), 0.000100); s != "0.000100" || ret != 8 {
+		t.Fatal("TestSnprintf:", s, ret)
 	}
 }
 
