ebe96344482855277b6b406a016a635ef8fe27e0
xushiwei <x@goplus.org>
2022-05-15 14:17:56 +0800

    qsort.TestMain

--- libtest_test.go
+++ libtest_test.go
@@ -3,12 +3,14 @@ package libc_test
 import (
 	"testing"
 
+	"github.com/goplus/libc/test/cmd/test/qsort"
 	"github.com/goplus/libc/test/cmd/test/string"
 	"github.com/goplus/libc/test/cmd/test/string_memcpy"
 	"github.com/goplus/libc/test/cmd/test/string_memset"
 )
 
 func TestFromTestdata(t *testing.T) {
+	qsort.TestMain(t)
 	string_memcpy.TestMain(t)
 	string_memset.TestMain(t)
 	string.TestMain(t)
