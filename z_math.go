6d214e50f26aa58b53c661b838c5cd6df71227e0
xushiwei <x@goplus.org>
2022-07-16 19:56:33 +0800

    public __builtin_inf/nan

--- z_math.go
+++ z_math.go
@@ -2,15 +2,18 @@ package libc
 
 import "math"
 
-func __builtin_inf() float64 {
+func X__builtin_inf() float64 {
 	return math.Inf(0)
 }
-func __builtin_inff() float32 {
-	return float32(__builtin_inf())
+
+func X__builtin_inff() float32 {
+	return float32(X__builtin_inf())
 }
-func __builtin_nan(*int8) float64 {
+
+func X__builtin_nan(*int8) float64 {
 	return math.NaN()
 }
-func __builtin_nanf(*int8) float32 {
+
+func X__builtin_nanf(*int8) float32 {
 	return float32(math.NaN())
 }
