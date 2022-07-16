56132168877b23a33933815de7e1060011c6dfbd
xushiwei <x@goplus.org>
2022-07-16 20:03:50 +0800

    test/strtod

--- libtest_test.go
+++ libtest_test.go
@@ -6,15 +6,18 @@ package libc_test
 import (
 	"testing"
 
+	teststring "github.com/goplus/libc/test/cmd/test/string"
+
 	"github.com/goplus/libc/test/cmd/test/qsort"
-	"github.com/goplus/libc/test/cmd/test/string"
 	"github.com/goplus/libc/test/cmd/test/string_memcpy"
 	"github.com/goplus/libc/test/cmd/test/string_memset"
+	"github.com/goplus/libc/test/cmd/test/strtod"
 )
 
 func TestFromTestdata(t *testing.T) {
 	qsort.TestMain(t)
 	string_memcpy.TestMain(t)
 	string_memset.TestMain(t)
-	string.TestMain(t)
+	teststring.TestMain(t)
+	strtod.TestMain(t)
 }
