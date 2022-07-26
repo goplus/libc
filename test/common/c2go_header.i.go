37a1730ffc8d40720511ec171babf895d471f52c
xushiwei <x@goplus.org>
2022-07-26 23:13:40 +0800

    libc/test/common

--- test/common/c2go_header.i.go
+++ test/common/c2go_header.i.go
@@ -2,270 +2,246 @@ package common
 
 import libc "github.com/goplus/libc"
 
-type struct_d_d struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	y    float64
-	dy   float32
-	e    int32
+type Struct_d_d struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	Y    float64
+	Dy   float32
+	E    int32
 }
-type Struct_d_d = struct_d_d
-type struct_f_f struct {
-	file *int8
-	line int32
-	r    int32
-	x    float32
-	y    float32
-	dy   float32
-	e    int32
+type Struct_f_f struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float32
+	Y    float32
+	Dy   float32
+	E    int32
 }
-type Struct_f_f = struct_f_f
-type struct_l_l struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	y    float64
-	dy   float32
-	e    int32
+type Struct_l_l struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	Y    float64
+	Dy   float32
+	E    int32
 }
-type Struct_l_l = struct_l_l
-type struct_ff_f struct {
-	file *int8
-	line int32
-	r    int32
-	x    float32
-	x2   float32
-	y    float32
-	dy   float32
-	e    int32
+type Struct_ff_f struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float32
+	X2   float32
+	Y    float32
+	Dy   float32
+	E    int32
 }
-type Struct_ff_f = struct_ff_f
-type struct_dd_d struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	x2   float64
-	y    float64
-	dy   float32
-	e    int32
+type Struct_dd_d struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	X2   float64
+	Y    float64
+	Dy   float32
+	E    int32
 }
-type Struct_dd_d = struct_dd_d
-type struct_ll_l struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	x2   float64
-	y    float64
-	dy   float32
-	e    int32
+type Struct_ll_l struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	X2   float64
+	Y    float64
+	Dy   float32
+	E    int32
 }
-type Struct_ll_l = struct_ll_l
-type struct_d_di struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	y    float64
-	dy   float32
-	i    int64
-	e    int32
+type Struct_d_di struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	Y    float64
+	Dy   float32
+	I    int64
+	E    int32
 }
-type Struct_d_di = struct_d_di
-type struct_f_fi struct {
-	file *int8
-	line int32
-	r    int32
-	x    float32
-	y    float32
-	dy   float32
-	i    int64
-	e    int32
+type Struct_f_fi struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float32
+	Y    float32
+	Dy   float32
+	I    int64
+	E    int32
 }
-type Struct_f_fi = struct_f_fi
-type struct_l_li struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	y    float64
-	dy   float32
-	i    int64
-	e    int32
+type Struct_l_li struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	Y    float64
+	Dy   float32
+	I    int64
+	E    int32
 }
-type Struct_l_li = struct_l_li
-type struct_di_d struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	i    int64
-	y    float64
-	dy   float32
-	e    int32
+type Struct_di_d struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	I    int64
+	Y    float64
+	Dy   float32
+	E    int32
 }
-type Struct_di_d = struct_di_d
-type struct_fi_f struct {
-	file *int8
-	line int32
-	r    int32
-	x    float32
-	i    int64
-	y    float32
-	dy   float32
-	e    int32
+type Struct_fi_f struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float32
+	I    int64
+	Y    float32
+	Dy   float32
+	E    int32
 }
-type Struct_fi_f = struct_fi_f
-type struct_li_l struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	i    int64
-	y    float64
-	dy   float32
-	e    int32
+type Struct_li_l struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	I    int64
+	Y    float64
+	Dy   float32
+	E    int32
 }
-type Struct_li_l = struct_li_l
-type struct_d_i struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	i    int64
-	e    int32
+type Struct_d_i struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	I    int64
+	E    int32
 }
-type Struct_d_i = struct_d_i
-type struct_f_i struct {
-	file *int8
-	line int32
-	r    int32
-	x    float32
-	i    int64
-	e    int32
+type Struct_f_i struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float32
+	I    int64
+	E    int32
 }
-type Struct_f_i = struct_f_i
-type struct_l_i struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	i    int64
-	e    int32
+type Struct_l_i struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	I    int64
+	E    int32
 }
-type Struct_l_i = struct_l_i
-type struct_d_dd struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	y    float64
-	dy   float32
-	y2   float64
-	dy2  float32
-	e    int32
+type Struct_d_dd struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	Y    float64
+	Dy   float32
+	Y2   float64
+	Dy2  float32
+	E    int32
 }
-type Struct_d_dd = struct_d_dd
-type struct_f_ff struct {
-	file *int8
-	line int32
-	r    int32
-	x    float32
-	y    float32
-	dy   float32
-	y2   float32
-	dy2  float32
-	e    int32
+type Struct_f_ff struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float32
+	Y    float32
+	Dy   float32
+	Y2   float32
+	Dy2  float32
+	E    int32
 }
-type Struct_f_ff = struct_f_ff
-type struct_l_ll struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	y    float64
-	dy   float32
-	y2   float64
-	dy2  float32
-	e    int32
+type Struct_l_ll struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	Y    float64
+	Dy   float32
+	Y2   float64
+	Dy2  float32
+	E    int32
 }
-type Struct_l_ll = struct_l_ll
-type struct_ff_fi struct {
-	file *int8
-	line int32
-	r    int32
-	x    float32
-	x2   float32
-	y    float32
-	dy   float32
-	i    int64
-	e    int32
+type Struct_ff_fi struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float32
+	X2   float32
+	Y    float32
+	Dy   float32
+	I    int64
+	E    int32
 }
-type Struct_ff_fi = struct_ff_fi
-type struct_dd_di struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	x2   float64
-	y    float64
-	dy   float32
-	i    int64
-	e    int32
+type Struct_dd_di struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	X2   float64
+	Y    float64
+	Dy   float32
+	I    int64
+	E    int32
 }
-type Struct_dd_di = struct_dd_di
-type struct_ll_li struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	x2   float64
-	y    float64
-	dy   float32
-	i    int64
-	e    int32
+type Struct_ll_li struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	X2   float64
+	Y    float64
+	Dy   float32
+	I    int64
+	E    int32
 }
-type Struct_ll_li = struct_ll_li
-type struct_fff_f struct {
-	file *int8
-	line int32
-	r    int32
-	x    float32
-	x2   float32
-	x3   float32
-	y    float32
-	dy   float32
-	e    int32
+type Struct_fff_f struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float32
+	X2   float32
+	X3   float32
+	Y    float32
+	Dy   float32
+	E    int32
 }
-type Struct_fff_f = struct_fff_f
-type struct_ddd_d struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	x2   float64
-	x3   float64
-	y    float64
-	dy   float32
-	e    int32
+type Struct_ddd_d struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	X2   float64
+	X3   float64
+	Y    float64
+	Dy   float32
+	E    int32
 }
-type Struct_ddd_d = struct_ddd_d
-type struct_lll_l struct {
-	file *int8
-	line int32
-	r    int32
-	x    float64
-	x2   float64
-	x3   float64
-	y    float64
-	dy   float32
-	e    int32
+type Struct_lll_l struct {
+	File *int8
+	Line int32
+	R    int32
+	X    float64
+	X2   float64
+	X3   float64
+	Y    float64
+	Dy   float32
+	E    int32
 }
-type Struct_lll_l = struct_lll_l
 
 func Checkexcept(got int32, want int32, r int32) int32 {
 	if r == int32(0) {
