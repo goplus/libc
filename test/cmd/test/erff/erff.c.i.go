90504db7bf7945b1f1e3f50651ef853da232ead1
xushiwei <x@goplus.org>
2022-08-04 20:39:18 +0800

    test/atan,atanh,cbrt,ceil,copysign,erf

---
+++ test/cmd/test/erff/erff.c.i.go
@@ -0,0 +1,51 @@
+package erff
+
+import (
+	common "github.com/goplus/libc/test/common"
+	unsafe "unsafe"
+	libc "github.com/goplus/libc"
+	testing "testing"
+)
+
+var _cgos_t_erff [15]common.Struct_f_f = [15]common.Struct_f_f{common.Struct_f_f{(*int8)(unsafe.Pointer(&[54]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'a', 'n', 'i', 't', 'y', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(1), int32(0), float32(-8.0668487548828125), float32(-1), float32(-3.1903298900121792e-23), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[54]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'a', 'n', 'i', 't', 'y', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(2), int32(0), float32(4.3452396392822266), float32(1), float32(0.0067037348635494709), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[54]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'a', 'n', 'i', 't', 'y', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(3), int32(0), float32(-8.3814334869384765), float32(-1), float32(-1.7388187936826997e-25), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[54]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'a', 'n', 'i', 't', 'y', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(4), int32(0), float32(-6.5316734313964844), float32(-1), float32(-2.1229585201332918e-13), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[54]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'a', 'n', 'i', 't', 'y', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(5), int32(0), float32(9.2670574188232421), float32(1), float32(2.4651903288156619e-32), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[54]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'a', 'n', 'i', 't', 'y', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(6), int32(0), float32(0.66198587417602539), float32(0.65082430839538574), float32(-0.2092125415802002), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[54]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'a', 'n', 'i', 't', 'y', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(7), int32(0), float32(-0.40660393238067627), float32(-0.43472546339035034), float32(0.30416014790534973), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[54]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'a', 'n', 'i', 't', 'y', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(8), int32(0), float32(0.56175976991653442), float32(0.57306551933288574), float32(0.38818889856338501), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[54]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'a', 'n', 'i', 't', 'y', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(9), int32(0), float32(0.77415227890014648), float32(0.72640305757522583), float32(0.45546483993530273), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[54]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'a', 'n', 'i', 't', 'y', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(10), int32(0), float32(-0.67876368761062622), float32(-0.66290289163589478), float32(0.41128727793693542), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[55]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'p', 'e', 'c', 'i', 'a', 'l', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(1), int32(0), float32(0), float32(0), float32(0), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[55]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'p', 'e', 'c', 'i', 'a', 'l', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(2), int32(0), float32(-0), float32(-0), float32(0), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[55]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'p', 'e', 'c', 'i', 'a', 'l', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(3), int32(0), libc.X__builtin_inff(), float32(1), float32(0), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[55]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'p', 'e', 'c', 'i', 'a', 'l', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(4), int32(0), -libc.X__builtin_inff(), float32(-1), float32(0), int32(0)}, common.Struct_f_f{(*int8)(unsafe.Pointer(&[55]int8{'/', 'U', 's', 'e', 'r', 's', '/', 'x', 'u', 's', 'h', 'i', 'w', 'e', 'i', '/', 'w', 'o', 'r', 'k', '/', 'l', 'i', 'b', 'c', '/', 't', 'e', 's', 't', '/', 's', 'r', 'c', '/', 'm', 'a', 't', 'h', '/', 's', 'p', 'e', 'c', 'i', 'a', 'l', '/', 'e', 'r', 'f', 'f', '.', 'h', '\x00'})), int32(5), int32(0), libc.X__builtin_nanf((*int8)(unsafe.Pointer(&[1]int8{'\x00'}))), libc.X__builtin_nanf((*int8)(unsafe.Pointer(&[1]int8{'\x00'}))), float32(0), int32(0)}}
+
+func _cgo_main() int32 {
+	var y float32
+	var d float32
+	var e int32
+	var i int32
+	var err int32 = int32(0)
+	var p *common.Struct_f_f
+	for i = int32(0); uint64(i) < 15; i++ {
+		p = (*common.Struct_f_f)(unsafe.Pointer(uintptr(unsafe.Pointer((*common.Struct_f_f)(unsafe.Pointer(&_cgos_t_erff)))) + uintptr(i)*32))
+		if p.R < int32(0) {
+			continue
+		}
+		libc.Fesetround(p.R)
+		libc.Feclearexcept(int32(0))
+		y = libc.Erff(p.X)
+		e = libc.Fetestexcept(0)
+		if !(common.Checkexcept(e, p.E, p.R) != 0) {
+			libc.Printf((*int8)(unsafe.Pointer(&[49]int8{'%', 's', ':', '%', 'd', ':', ' ', 'b', 'a', 'd', ' ', 'f', 'p', ' ', 'e', 'x', 'c', 'e', 'p', 't', 'i', 'o', 'n', ':', ' ', '%', 's', ' ', 'e', 'r', 'f', 'f', '(', '%', 'a', ')', '=', '%', 'a', ',', ' ', 'w', 'a', 'n', 't', ' ', '%', 's', '\x00'})), p.File, p.Line, common.Rstr(p.R), float64(p.X), float64(p.Y), common.Estr(p.E))
+			libc.Printf((*int8)(unsafe.Pointer(&[9]int8{' ', 'g', 'o', 't', ' ', '%', 's', '\n', '\x00'})), common.Estr(e))
+			err++
+		}
+		d = common.Ulperrf(y, p.Y, p.Dy)
+		if !(common.Checkulp(d, p.R) != 0) {
+			libc.Printf((*int8)(unsafe.Pointer(&[57]int8{'%', 's', ':', '%', 'd', ':', ' ', '%', 's', ' ', 'e', 'r', 'f', 'f', '(', '%', 'a', ')', ' ', 'w', 'a', 'n', 't', ' ', '%', 'a', ' ', 'g', 'o', 't', ' ', '%', 'a', ' ', 'u', 'l', 'p', 'e', 'r', 'r', ' ', '%', '.', '3', 'f', ' ', '=', ' ', '%', 'a', ' ', '+', ' ', '%', 'a', '\n', '\x00'})), p.File, p.Line, common.Rstr(p.R), float64(p.X), float64(p.Y), float64(y), float64(d), float64(d-p.Dy), float64(p.Dy))
+			err++
+		}
+	}
+	return func() int32 {
+		if !!(err != 0) {
+			return 1
+		} else {
+			return 0
+		}
+	}()
+}
+func TestMain(t *testing.T) {
+	if _cgo_ret := _cgo_main(); _cgo_ret != 0 {
+		t.Fatal("exit status", _cgo_ret)
+	}
+}
